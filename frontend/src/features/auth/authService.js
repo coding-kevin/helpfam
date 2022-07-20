dfcea1bf280ed8f3629e9369a9879adec916d2ab
coding-kevin <kevinmcl1277@icloud.com>
2022-07-19 18:22:27 -0700

    fixed API_URL in authService and ticketService

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "https://kevin-tickets-backend.herokuapp.com/api";
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://kevin-tickets-backend.herokuapp.com/api";
+// const API_URL = "http://localhost:8000/api";
 
 const register = async (userData) => {
   const response = await axios.post(API_URL + "/users/register", userData);
--- frontend/src/features/tickets/ticketsService.js
+++ frontend/src/features/tickets/ticketsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_URL = "https://kevin-tickets-backend.herokuapp.com/api";
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://kevin-tickets-backend.herokuapp.com/api";
+// const API_URL = "http://localhost:8000/api";
 //Create a Ticket
 const createTicket = async (ticketData, token) => {
   const config = {
